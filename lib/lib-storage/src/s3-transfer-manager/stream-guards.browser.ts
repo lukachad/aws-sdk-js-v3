e226b9ed8004f415e693f2296b366c2881fe6d95
Lukas Chang <lukachad@amazon.com>
2025-06-19 20:29:45 +0000

    feat(lib-storage): transfermanager interation post pair programming

--- lib/lib-storage/src/s3-transfer-manager/stream-guards.browser.ts
+++
@@ -1,6 +0,0 @@
-export function isWebStream(stream: unknown): stream is ReadableStream | Blob {
-  return (
-    (typeof ReadableStream !== "undefined" && stream instanceof ReadableStream) ||
-    (typeof Blob !== "undefined" && stream instanceof Blob)
-  );
-}
