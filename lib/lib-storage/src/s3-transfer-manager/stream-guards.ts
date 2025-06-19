6bd8f6ce2b037532d89a48958be0d05a769728c3
Lukas Chang <lukachad@amazon.com>
2025-06-19 20:29:45 +0000

    feat(lib-storage): transfermanager interation post pair programming

--- lib/lib-storage/src/s3-transfer-manager/stream-guards.ts
+++ lib/lib-storage/src/s3-transfer-manager/stream-guards.ts
@@ -1,6 +1,12 @@
-import { Readable } from "node:stream";
-
 // * confirm if filestream fits here *
+// will not work with browser because no readable in browser
 export function isNodeStream(stream: unknown): stream is Readable {
-  return stream instanceof Readable || (typeof stream === "object" && stream !== null && "pipe" in stream);
+  return typeof stream === "object" && stream !== null && "pipe" in stream && typeof stream.pipe === "function";
+}
+
+export function isWebStream(stream: unknown): stream is ReadableStream | Blob {
+  return (
+    (typeof ReadableStream !== "undefined" && stream instanceof ReadableStream) ||
+    (typeof Blob !== "undefined" && stream instanceof Blob)
+  );
 }
