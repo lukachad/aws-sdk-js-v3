676de4ad5579cb916f89fad76e50a1ebac59e307
Lukas Chang <lukachad@amazon.com>
2025-06-24 20:15:30 +0000

    feat(lib-storage): bug fixes and test env setup

---
+++ lib/lib-storage/src/s3-transfer-manager/S3TransferManager.spec.ts
@@ -0,0 +1,7 @@
+import { describe, expect, test as it } from "vitest";
+
+import { S3TransferManager } from "./S3TransferManager";
+
+describe(S3TransferManager.name, () => {
+  it("should do something", async () => {});
+});
