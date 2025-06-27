17feb3624a7dcf53070d6151aa4d7a8fcd85b39a
Lukas Chang <lukachad@amazon.com>
2025-06-27 16:56:05 +0000

    feat(lib-storage): implemented dispatchEvent(), need to add dispatches for complete and fail

--- lib/lib-storage/src/s3-transfer-manager/join-streams.ts
+++ lib/lib-storage/src/s3-transfer-manager/join-streams.ts
@@ -1,9 +1,18 @@
+import { DownloadRequest } from "@aws-sdk/lib-lib-storage/dist-types/s3-transfer-manager/types";
 import { StreamingBlobPayloadOutputTypes } from "@smithy/types";
 import { isBlob, isReadableStream, sdkStreamMixin } from "@smithy/util-stream";
+import { request } from "http";
 import { Readable } from "stream";
 
 // check all types. needs to join nodejs and browser together
-export function joinStreams(streams: StreamingBlobPayloadOutputTypes[]): StreamingBlobPayloadOutputTypes {
+export function joinStreams(
+  streams: StreamingBlobPayloadOutputTypes[],
+  transferInfo?: {
+    request: DownloadRequest;
+    dispatchEvent?: (event: Event) => boolean;
+    totalBytes?: number;
+  }
+): StreamingBlobPayloadOutputTypes {
   // console.log("Is Readable Stream: ");
   // console.log(isReadableStream(streams[0]));
 
@@ -22,14 +31,19 @@ export function joinStreams(streams: StreamingBlobPayloadOutputTypes[]): Streami
   } else if (isBlob(streams[0])) {
     throw new Error("Blob not supported yet");
   } else {
-    return sdkStreamMixin(Readable.from(iterateStreams(streams)));
+    return sdkStreamMixin(Readable.from(iterateStreams(streams, transferInfo)));
   }
 }
 
 export async function* iterateStreams(
-  streams: StreamingBlobPayloadOutputTypes[]
+  streams: StreamingBlobPayloadOutputTypes[],
+  transferInfo?: {
+    request: DownloadRequest;
+    dispatchEvent?: (event: Event) => boolean;
+    totalBytes?: number;
+  }
 ): AsyncIterable<StreamingBlobPayloadOutputTypes, void, void> {
-  let total = 0;
+  let bytesTransferred = 0;
   for (const stream of streams) {
     if (isReadableStream(stream)) {
       const reader = stream.getReader();
@@ -39,7 +53,7 @@ export async function* iterateStreams(
           break;
         }
         yield value;
-        total += value.byteLength;
+        bytesTransferred += value.byteLength;
       }
       reader.releaseLock();
     } else if (isBlob(stream)) {
@@ -47,6 +61,20 @@ export async function* iterateStreams(
     } else if (stream instanceof Readable) {
       for await (const chunk of stream) {
         yield chunk;
+        const chunkSize = Buffer.isBuffer(chunk) ? chunk.length : Buffer.byteLength(chunk);
+        bytesTransferred += chunkSize;
+
+        if (transferInfo?.request && transferInfo?.dispatchEvent) {
+          transferInfo.dispatchEvent(
+            Object.assign(new Event("bytesTransferred"), {
+              request: transferInfo.request,
+              snapshot: {
+                transferredBytes: bytesTransferred,
+                totalBytes: transferInfo.totalBytes,
+              },
+            })
+          );
+        }
       }
     } else {
       throw new Error("unhandled stream type", stream);
