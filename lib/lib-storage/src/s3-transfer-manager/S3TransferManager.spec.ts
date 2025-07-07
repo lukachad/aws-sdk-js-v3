1c592b2cfb79c1be365a26361ea0ada65ad22082
Lukas Chang <lukachad@amazon.com>
2025-07-07 18:14:30 +0000

    feat(lib-storage): added check in validateExpectedRanges if final part doesnt download total object

--- lib/lib-storage/src/s3-transfer-manager/S3TransferManager.spec.ts
+++ lib/lib-storage/src/s3-transfer-manager/S3TransferManager.spec.ts
@@ -1,6 +1,16 @@
-import { describe, expect, test as it, vi } from "vitest";
+import { beforeAll, describe, expect, test as it, vi } from "vitest";
 
 import { S3TransferManager } from "./S3TransferManager";
+
+/**
+ * Unit Tests:
+ * - addEventListener()
+ * - dispatchEvent()
+ * - removeEventListener()
+ * - TM Constructor
+ * - *iterateListeners()
+ * - validateExpectedRanges()
+ */
 
 describe("S3TransferManager Unit Tests", () => {
   describe("validateExpectedRanges()", () => {
@@ -14,20 +24,32 @@ describe("S3TransferManager Unit Tests", () => {
         "bytes 5242880-10485759/13631488",
         "bytes 10485760-13631487/13631488",
       ];
-      const totalSize = 13631488;
 
       for (let i = 1; i < ranges.length; i++) {
         expect(() => {
-          tm.validateExpectedRanges(ranges[i - 1], ranges[i], i + 1, totalSize);
+          tm.validateExpectedRanges(ranges[i - 1], ranges[i], i + 1);
         }).not.toThrow();
       }
+    });
+    it("Should throw error for incomplete download", () => {
+      const ranges = [
+        "bytes 0-5242879/13631488",
+        "bytes 5242880-10485759/13631488",
+        "bytes 10485760-13631480/13631488", // 8 bytes short
+      ];
+
+      expect(() => {
+        tm.validateExpectedRanges(ranges[1], ranges[2], 3);
+      }).toThrow(
+        "Range validation failed: Final part did not cover total range of 13631488. Expected range of bytes 10485760-314572"
+      );
     });
     it("Should throw error for non-sequential ranges", () => {
       const previousRange = "bytes 0-5242879/13631488";
       const invalidRange = "bytes 5242881-10485759/13631488"; // 1 byte off
 
       expect(() => {
-        tm.validateExpectedRanges(previousRange, invalidRange, 2, 13631488);
+        tm.validateExpectedRanges(previousRange, invalidRange, 2);
       }).toThrow("Expected part 2 to start at 5242880 but got 5242881");
     });
     it("Should throw error for non-sequential ranges", () => {
@@ -35,7 +57,7 @@ describe("S3TransferManager Unit Tests", () => {
       const invalidRange = "bytes 5242879-10485759/13631488";
 
       expect(() => {
-        tm.validateExpectedRanges(previousRange, invalidRange, 2, 13631488);
+        tm.validateExpectedRanges(previousRange, invalidRange, 2);
       }).toThrow("Expected part 2 to start at 5242880 but got 5242879");
     });
     it("Should throw error for non-sequential ranges", () => {
@@ -43,7 +65,15 @@ describe("S3TransferManager Unit Tests", () => {
       const invalidRange = "bytes 0-5242879/13631488";
 
       expect(() => {
-        tm.validateExpectedRanges(previousRange, invalidRange, 2, 13631488);
+        tm.validateExpectedRanges(previousRange, invalidRange, 2);
+      }).toThrow("Expected part 2 to start at 5242880 but got 0");
+    });
+    it("Should throw error for non-sequential ranges", () => {
+      const previousRange = "bytes 0-5242879/13631488";
+      const invalidRange = "bytes 0-5242879/13631488";
+
+      expect(() => {
+        tm.validateExpectedRanges(previousRange, invalidRange, 2);
       }).toThrow("Expected part 2 to start at 5242880 but got 0");
     });
   });
